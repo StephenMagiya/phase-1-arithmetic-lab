2d52780eb6d6c995b7a83404a04a7951ec731a60
stephen <stephenmagiya97@gmail.com>
2025-03-13 00:14:02 +0300

    creates variables to equations

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 8
+let num4 = 2
+
+let mod = num3 / num4;
+
+let max = Math.max(2, 5, 7, 9, 20);
+
 
 // Write your code here
+
